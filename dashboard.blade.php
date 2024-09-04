20a5e5dfaa80fe0e9907b2d166fc6eb6c078a208
ramushrma <169146925+ramushrma@users.noreply.github.com>
2024-09-04 19:23:43 +0530

    Add files via upload

---
+++ dashboard.blade.php
@@ -0,0 +1,150 @@
+@extends('admin.main')
+@section('dashboard')
+<div class="containser fluid ms-1">
+    <div class="row page-titles mx-0">
+        <div class="col-sm-6 p-md-0">
+            <div class="welcome-text">
+                <h4>Hi Ludo Zupee !!!!</h4>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-3 col-sm-6">
+            <div class="card shadow-lg">
+                <div class="stat-widget-one card-body">
+                    <div class="stat-icon d-inline-block">
+                        <i class="ti-export text-danger border-info "></i>
+                    </div>
+                    <div class="stat-content d-inline-block">
+                          <h6>Total Withdrawal</h6>
+                        <div class="stat-digit">11,200</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="card shadow-lg">
+                <div class="stat-widget-one card-body">
+                    <div class="stat-icon d-inline-block">
+                        <i class="ti-import text-danger border-info"></i>
+                    </div>
+                    <div class="stat-content d-inline-block">
+                        <h6>Today Deposit</h6>
+                        <div class="stat-digit">4,560</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="card shadow-lg">
+                <div class="stat-widget-one card-body">
+                    <div class="stat-icon d-inline-block">
+                        <i class="ti-user  border-danger text-info "></i>
+                    </div>
+                    <div class="stat-content d-inline-block">
+                        <h6>All Users</h6>
+                        <div class="stat-digit">45,646</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="card shadow-lg">
+                <div class="stat-widget-one card-body">
+                    <div class="stat-icon d-inline-block">
+                        <i class="ti-cup  border-danger text-info "></i>
+                    </div>
+                    <div class="stat-content d-inline-block">
+                    <h6>Referal</h6>
+                        <div class="stat-digit">2,123</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-3 col-sm-6">
+            <div class="card shadow-lg">
+                <div class="stat-widget-one card-body">
+                    <div class="stat-icon d-inline-block">
+                        <i class="ti-money border-danger text-info "></i>
+                    </div>
+                    <div class="stat-content d-inline-block">
+                    <h6>Total Bonus</h6>
+                        <div class="stat-digit">6,012</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="card">
+                <div class="stat-widget-one card-body">
+                    <div class="stat-icon d-inline-block">
+                        <i class="ti-star border-danger text-info "></i>
+                    </div>
+                    <div class="stat-content d-inline-block">
+                    <h6>Costomer</h6>
+                        <div class="stat-digit">6,102</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="card shadow-lg">
+                <div class="stat-widget-one card-body">
+                    <div class="stat-icon d-inline-block">
+                        <i class="ti-game text-danger border-info"></i>
+                    </div>
+                    <div class="stat-content d-inline-block">
+                    <h6>Game Played</h6>
+                        <div class="stat-digit">770</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="card shadow-lg">
+                <div class="stat-widget-one card-body">
+                    <div class="stat-icon d-inline-block">
+                        <i class="ti-link text-danger border-info"></i>
+                    </div>
+                    <div class="stat-content d-inline-block">
+                    <h6>Total Referral</h6>
+                        <div class="stat-digit">2,781</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-3 col-sm-6">
+            <div class="card shadow-lg">
+                <div class="stat-widget-one card-body">
+                    <div class="stat-icon d-inline-block">
+                        <i class="ti-vimeo-alt text-danger border-info"></i>
+                    </div>
+                    <div class="stat-content d-inline-block">
+                    <h6>Profit</h6>
+                        <div class="stat-digit">7,708</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="card shadow-lg">
+                <div class="stat-widget-one card-body">
+                    <div class="stat-icon d-inline-block">
+                        <i class="ti-wordpress text-danger border-info"></i>
+                    </div>
+                    <div class="stat-content d-inline-block">
+                    <h6>Viners</h6>
+                        <div class="stat-digit">8,781</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+  </div>
+@endsection
